405f0db92acc242bb6f3e6d4fe7de6bc23489ce8
Walker Chesley <chesley.walker@gmail.com>
2020-09-14 16:12:10 -0500

    updated index
    updated to verify reads from the 'database' work.

--- CodingClub/Pages/Index.cshtml.cs
+++ CodingClub/Pages/Index.cshtml.cs
@@ -6,20 +6,23 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 
+
 namespace CodingClub.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly CodingClub.Data.CodingClubContext _context; 
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, CodingClub.Data.CodingClubContext context)
         {
             _logger = logger;
+            _context = context; 
         }
 
         public void OnGet()
         {
-
+            IQueryable<ClubMember> members = from m in _context.ClubMember select m; 
         }
     }
 }
