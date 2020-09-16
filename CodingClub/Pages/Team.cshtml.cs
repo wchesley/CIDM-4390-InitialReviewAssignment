7cea3a68211834073eb7e73bb52a7c832daec2ba
Walker Chesley <chesley.walker@gmail.com>
2020-09-16 15:50:24 -0500

    added page & model to view all teams

--- CodingClub/Pages/Team.cshtml.cs
+++ CodingClub/Pages/Team.cshtml.cs
@@ -5,25 +5,30 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore; 
 
 namespace CodingClub.Pages
 {
     public class Team : PageModel
     {
-        private readonly ILogger<Team> _logger; 
-        private readonly CodingClub.Data.CodingClubContext _context; 
-        public IQueryable<Teams> TeamData; 
+        private readonly ILogger<Team> _logger;
+        private readonly CodingClub.Data.CodingClubContext _context;
+        public List<Teams> TeamData { get; set; }
 
         public Team(ILogger<Team> logger, CodingClub.Data.CodingClubContext context)
         {
-            _logger = logger; 
-            _context = context; 
+            _logger = logger;
+            _context = context;
         }
 
         //Will prolly need a parameter here to get the specific teams page: 
-        public void OnGet()
+        public async Task<IActionResult> OnGet()
         {
-            TeamData = from t in _context.Teams select t; 
+            TeamData = await (from t in _context.Teams
+            orderby t.TeamID
+            select t).ToListAsync();
+            return Page(); 
         }
     }
 }
