6756052247345096a34cf68026f1df81a94ed715
Walker Chesley <chesley.walker@gmail.com>
2020-09-16 22:26:02 -0500

    added ability to delete teams

--- CodingClub/Pages/Team.cshtml.cs
+++ CodingClub/Pages/Team.cshtml.cs
@@ -14,6 +14,7 @@ namespace CodingClub.Pages
     {
         private readonly ILogger<Team> _logger;
         private readonly CodingClub.Data.CodingClubContext _context;
+        [BindProperty]
         public List<Teams> TeamData { get; set; }
 
         public Team(ILogger<Team> logger, CodingClub.Data.CodingClubContext context)
@@ -23,12 +24,31 @@ namespace CodingClub.Pages
         }
 
         //Will prolly need a parameter here to get the specific teams page: 
-        public async Task<IActionResult> OnGet()
+        public async Task<IActionResult> OnGetAsync()
         {
-            TeamData = await (from t in _context.Teams
+            TeamData = await (from t in _context.Teams.Include(cm => cm.TeamLeader).AsNoTracking()
                               orderby t.TeamID
                               select t).ToListAsync();
             return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? TeamID)
+        {
+            if(TeamID == null)
+            {
+                return NotFound(); 
+            }
+            Teams teamToDelete = await _context.Teams.FindAsync(TeamID); 
+            try
+            {
+                _context.Teams.Remove(teamToDelete); 
+                await _context.SaveChangesAsync(); 
+            }
+            catch(Exception e)
+            {
+                _logger.LogWarning(e, "Error deleting Team.");
+            }
+            return RedirectToPage("Team");
         }
     }
 }
